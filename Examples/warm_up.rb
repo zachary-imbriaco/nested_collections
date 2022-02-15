c02e560d46883b0a52dea1529a75336c5ba7415e
Zachary Imbriaco <49922103+zachary-imbriaco@users.noreply.github.com>
2022-02-15 11:06:18 -0500

    Update warm_up.rb

--- Examples/warm_up.rb
+++ Examples/warm_up.rb
@@ -1,12 +1,19 @@
 animals = {"dogs" => 3, "cats" => 5, "iguanas" => 2}
 
 # 1. return the value of dogs
+p animals["dogs"]
 # 2. add 3 parakeets
+animals["parakeets"] = 3
 # 3. increase the amount of cats by 2
+animals["cats"] += 2
 
 pet_names = ["Fela", "Spot", "Patch", "Willy"]
 
 # 1. add “Claude” to the end of the array
+pet_names.append("Claude")
 # 2. access the first name in the list
+pet_names[0]
 # 3. access the last name in the list
+pet_names[-1]
 # 4. remove “Fela” from the list
+pet_names.shift
